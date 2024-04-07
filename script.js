56c7a6d1f6106a81e52989956875b75b9723baa5
GalimovMaxim <162039054+GalimovMaxim@users.noreply.github.com>
2024-04-07 19:35:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function fun2() {
+    //alert("Нажата кнопка запись (через addEventListener), меняем цвет фона одного из абзацев и текст");
+    //text2 = document.querySelector("#text2")
+    //text2.innerHTML = 'Изменный текст'
+    //text2.style.backgroundColor ='aqua';
+}
+
+but2 = document.querySelector("#dr")
+but2.addEventListener('click', fun2)
