7fcdd04222cd0d65767f68e4b57f227024a9f908
GalimovMaxim <162039054+GalimovMaxim@users.noreply.github.com>
2024-04-14 19:11:06 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 function fun2() {
-    //alert("Нажата кнопка запись (через addEventListener), меняем цвет фона одного из абзацев и текст");
-    //text2 = document.querySelector("#text2")
-    //text2.innerHTML = 'Изменный текст'
-    //text2.style.backgroundColor ='aqua';
+    alert("Здесь представлены события на этот празднычный день.");
+    text2 = document.querySelector("#info")
+    text2.innerHTML = 'День Весны и Труда. Этот день считается символом весны, он олицетворяет собой уважение к любому созидательному труду. Традиция встречать этот праздник насчитывает уже много лет.'
+   
+
 }
 
-but2 = document.querySelector("#dr")
+function fun3() {
+    alert("Здесь представлены события на этот празднычный день.");
+    text3 = document.querySelector("#info")
+    text3.innerHTML = 'День Победы. Праздник победы Красной армии и советского народа над нацистской Германией в Великой Отечественной войне 1941—1945 годов.'
+   
+
+}
+
+but2 = document.querySelector("#m1")
 but2.addEventListener('click', fun2)
+
+
+but2 = document.querySelector("#m9")
+but2.addEventListener('click', fun3)
+labek.addEventListener("")
